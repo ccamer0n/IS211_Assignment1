09f9379b9b231e9909bc622d5d97f73f4ac204a8
ccamer0n <christopher.cameron77@spsmail.cuny.edu>
2023-01-30 22:55:42 -0500

    Added function docstrings

--- assignment1_part2.py
+++ assignment1_part2.py
@@ -1,15 +1,16 @@
 class Book:
-    author = ''
-    title = ''
     def __init__(self, author, title):
+        '''Constructor of the Book class'''
         self.author = author
         self.title = title
     #@classmethod
     def display(Book):
+        '''Prints the book title and author'''
         print(f"{Book.title}, written by {Book.author}")
         
-Book_1 = Book("J. K. Rowling", "Harry Potter and the Goblet of Fire")
-Book_2 = Book("Walter Scott", "Ivanhoe: A Romance")
+if __name__ == "__main__":
+    Book_1 = Book("J. K. Rowling", "Harry Potter and the Goblet of Fire")
+    Book_2 = Book("Walter Scott", "Ivanhoe: A Romance")
 
-Book_1.display()
-Book_2.display()
+    Book_1.display()
+    Book_2.display()
